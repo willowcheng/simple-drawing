ec76540df03a652f693cd6e0692b608020876550
willowcheng <e.wingus@Gmail.com>
2015-09-01 19:57:37 -0400

    Add new color

--- js/app.js
+++ js/app.js
@@ -4,37 +4,44 @@
 var color = $(".selected").css("background-color");
 
 //When clicking on control list items
-$(".controls li").click(function () {
-  //Deselect sibling elements
-  $(this).siblings().removeClass("selected");
-  //Select clicked elements
-  $(this).addClass("selected");
-  //Cache current color
-  color = $("this").css("background-color");
+$(".controls").on("click", "li", function () {
+    //Deselect sibling elements
+    $(this).siblings().removeClass("selected");
+    //Select clicked elements
+    $(this).addClass("selected");
+    //Cache current color
+    color = $("this").css("background-color");
 });
 
 //When new color is pressed
 $("#revealColorSelect").click(function () {
-  //Show color select or hide the color select
-  changeColor();
-  $("#colorSelect").toggle();
+    //Show color select or hide the color select
+    changeColor();
+    $("#colorSelect").toggle();
 });
 
 //Update the new color span
 function changeColor() {
-  var r = $("#red").val();
-  var g = $("#green").val();
-  var b = $("#blue").val();
+    var r = $("#red").val();
+    var g = $("#green").val();
+    var b = $("#blue").val();
 
-  $("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+    $("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
 }
 
 //When color sliders change
 $("input[type=range]").change(changeColor).mousemove(changeColor);
 
-//When add color is pressed
-  //Append the color to the controls ul
-  //Select the new color
+//When "Add Color" is pressed
+$("#addNewColor").click(function () {
+    //Append the color to the controls ul
+    var $newColor = $("<li></li>");
+    $newColor.css("background-color", $("#newColor").css("background-color"));
+    $(".controls ul").append($newColor);
+    //Select the new color
+    $newColor.click();
+
+});
 
 //On mouse events on the canvas
-  //Draw lines
+//Draw lines
